20e4a1f40d0f38abe0e59421f0bda765b52382c2
Daniel <danhos2810@gmail.com>
2021-09-17 01:08:58 +0700

    change ssl

--- db.js
+++ db.js
@@ -5,7 +5,9 @@ const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPAS
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: isProduction,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 // const pool = new Pool({
