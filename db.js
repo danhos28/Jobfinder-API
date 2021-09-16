ecf39854a9f8cfc8363d9922b10d3188e45b9b5f
Daniel <danhos2810@gmail.com>
2021-09-17 01:02:47 +0700

    change db connect

--- db.js
+++ db.js
@@ -1,11 +1,19 @@
 const { Pool } = require('pg');
+const isProduction = process.env.NODE_ENV === 'production';
+
+const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`;
 
 const pool = new Pool({
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction,
 });
+
+// const pool = new Pool({
+//   user: process.env.PGUSER,
+//   password: process.env.PGPASSWORD,
+//   database: process.env.PGDATABASE,
+//   host: process.env.PGHOST,
+//   port: process.env.PGPORT,
+// });
 
 module.exports = pool;
