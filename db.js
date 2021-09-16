dd541d4984aee5e3db2af11b9539c8d59882b9c5
Daniel <danhos2810@gmail.com>
2021-09-17 02:54:45 +0700

    add ssl

--- db.js
+++ db.js
@@ -5,10 +5,10 @@ const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPAS
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  // ssl: {
-  //   require: true,
-  //   rejectUnauthorized: false,
-  // },
+  ssl: {
+    require: true,
+    rejectUnauthorized: false,
+  },
 });
 
 // const pool = new Pool({
